92813eddc16d6a31c5d2480859e696ff3f950be4
Eric Faehnrich <eric@faehnri.ch>
2015-01-09 16:13:45 -0500

    initial commit, add solution and project files and first source for ACH

---
+++ NACHO/ACH.cs
@@ -0,0 +1,15 @@
+﻿/*
+ * NACHO - National Automated Clearing House Objects
+ * 
+ * A library for the NACHA ACH input file structure
+ */
+namespace NACHO
+{
+    /*
+     * ACH Input File Structure
+     * 
+     */
+    public class ACH
+    {
+    }
+}
